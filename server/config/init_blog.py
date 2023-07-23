450e090bd116457c8dc4d049a8b457f4b42da7f1
Kris <KrisShin5014@Outlook.com>
2023-07-23 15:56:43 +0800

    config server.

---
+++ server/config/init_blog.py
@@ -0,0 +1,86 @@
+from tortoise.contrib.fastapi import register_tortoise
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from redis import asyncio as aioredis
+
+from config.settings import (
+    REDIS_DB,
+    REDIS_HOST,
+    REDIS_PASS,
+    REDIS_PORT,
+    REDIS_USER,
+    TORTOISE_ORM,
+)
+
+
+# from user.apis import router as user_router
+
+# def register_redis(app: FastAPI):
+#     @app.on_event("startup")
+#     async def startup_event():
+#         app.state.redis = await aioredis.from_url(
+#             f"redis://{REDIS_USER}:{REDIS_PASS}@{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}?encoding=utf-8"
+#         )
+#
+#     @app.on_event("shutdown")
+#     async def shutdown_event():
+#         await app.state.redis.close()
+
+
+def init_db(app):
+    register_tortoise(
+        app,
+        config=TORTOISE_ORM,
+        generate_schemas=True,
+        add_exception_handlers=True,
+    )
+
+
+def register_router(app):
+    # app.include_router(
+    #     user_router,
+    #     tags=['user'],
+    #     responses={404: {'description': 'Not Found'}},
+    #     prefix='/api/user',
+    # )
+    # app.include_router(
+    #     project_router,
+    #     tags=['project'],
+    #     responses={404: {'description': 'Not Found'}},
+    #     prefix='/api/project',
+    # )
+    # app.include_router(
+    #     test_router,
+    #     tags=['test'],
+    #     responses={404: {'description': 'Not Found'}},
+    #     prefix="/api/test",
+    # )
+    ...
+
+
+def create_app():
+    app = FastAPI()
+    origins = [
+        "http://localhost",
+        "http://localhost:9100",
+    ]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+        allow_origin_regex=r"http://localhost:\d+",
+    )
+
+    app.mount("/static", StaticFiles(directory="server/statics"), name="static")
+
+    init_db(app)
+
+    # register_redis(app)
+
+    register_router(app)
+
+    return app
---
+++ server/config/settings.py
@@ -0,0 +1,41 @@
+from dotenv import dotenv_values
+
+# 加载 .env 文件中的环境变量
+env = dotenv_values('.env')
+
+PG_HOST = env.get('PG_HOST')
+PG_PORT = env.get('PG_PORT')
+PG_USER = env.get('PG_USER')
+PG_PASS = env.get('PG_PASS')
+PG_DB = env.get('PG_DB')
+
+REDIS_HOST = env.get('REDIS_HOST')
+REDIS_PORT = env.get('REDIS_PORT')
+REDIS_USER = env.get('REDIS_USER')
+REDIS_PASS = env.get('REDIS_PASS')
+REDIS_DB = env.get('REDIS_DB')
+
+TORTOISE_ORM = {
+    "connections": {
+        "default": f"postgres://{PG_USER}:{PG_PASS}@{PG_HOST}:{PG_PORT}/{PG_DB}"
+    },
+    "apps": {
+        "models": {
+            "models": [
+                'aerich.models',
+            ],
+            "default_connection": "default",
+        },
+    },
+}
+
+# to get a string like this run:
+# openssl rand -hex 32
+SECRET_KEY = env.get('SECRET_KEY')
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_DAYS = 7
+
+HTTP_ADDR = 'http://localhost'
+HTTP_PORT = 9100
+HTTP_SITE = f'{HTTP_ADDR}:{HTTP_PORT}'
+DEFAULT_AVATAR = f'{HTTP_SITE}/static/avatar/default.jpg'
